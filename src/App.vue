52b259ee4d8658b5fcf0e345b6325c13e2de05b6
Yan <mm7246591@yahoo.com.tw>
2022-03-09 23:33:35 +0800

    Update loading

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <router-view></router-view>
+  <div v-loading="isLoading" element-loading-background="rgba(0, 0, 0, 0.7)">
+    <router-view></router-view>
+  </div>
 </template>
 <script>
+import { computed } from "@vue/runtime-core";
+import store from "./store";
 export default {
   name: "App",
   components: {},
+  setup() {
+    const isLoading = computed(() => {
+      return store.state.isLoading;
+    });
+    return { isLoading };
+  },
 };
 </script>
 <style scoped>
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,19 @@ const store = createStore({
     state() {
         return {
             items: [],
+            status: false,
             isLoading: false,
         };
     },
     getters: {
         items: (state) => {
             return state.items;
+        },
+        status: (state) => {
+            return state.status;
+        },
+        isLoading: (state) => {
+            return state.isLoading;
         },
         menItems(state) {
             return state.items.filter((item) => item.category.includes("男"));
@@ -23,17 +30,30 @@ const store = createStore({
     },
     actions: {
         async getData({ commit }) {
+            commit("LOADING", true);
             try {
                 commit("GET_DATA", await getItems());
+                await setTimeout(() => {
+                    commit("LOADING", false);
+                }, 500);
             } catch (err) {
                 console.error(err);
             }
         },
+
+        updateLoading({ commit }) {
+            commit("LOADING", store.state);
+        },
     },
     mutations: {
         GET_DATA(state, payload) {
-            state.isLoading = !state.isLoading;
             state.items = payload.items;
+        },
+        LOADING(state, status) {
+            state.isLoading = status;
+        },
+        STATUS(state, payload) {
+            state.status = payload;
         },
     },
 });
